72339ef42dec15db1087d00f288cefbbfd1320a5
Hwan616 <hoan0616@naver.com>
2026-03-20 17:05:27 +0900

    Fix: page address

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-home_page = st.Page("home.py", title="Home", default=True)
-graphiti_page = st.Page("1_Graphiti_Agent.py", title="Graphiti Agent")
-visualization_page = st.Page("2_Graph_Visualization.py", title="Graph Visualization")
-comparison_page = st.Page("3_Comparison_LangGraph.py", title="Comparison with LangGraph")
+home_page = st.Page("./home.py", title="Home", default=True)
+graphiti_page = st.Page("./1_Graphiti_Agent.py", title="Graphiti Agent")
+visualization_page = st.Page("./2_Graph_Visualization.py", title="Graph Visualization")
+comparison_page = st.Page("./3_Comparison_LangGraph.py", title="Comparison with LangGraph")
 
 pg = st.navigation([home_page, graphiti_page, visualization_page, comparison_page])
 
